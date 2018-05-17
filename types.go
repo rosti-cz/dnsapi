153b6c84dd8919218fc0f0564bca7cbf3067ac0e
Adam Štrauch <cx@initd.cz>
2018-05-17 03:10:18 +0200

    Enhanced validation

--- types.go
+++ types.go
@@ -20,6 +20,10 @@ type Record struct {
 
 // Validates the record
 func (r *Record) Validate() error {
+	if r.TTL < 60 && r.TTL > 2592000 {
+		return errors.New(r.Name + ": TTL has to be number between 60 and 2592000")
+	}
+
 	if r.Type == "A" {
 		parsed := net.ParseIP(r.Value)
 
@@ -41,9 +45,11 @@ func (r *Record) Validate() error {
 			return errors.New(r.Name + ": CNAME has not a valid value")
 		}
 	} else if r.Type == "TXT" {
+		if strings.Contains(r.Value, "\"") || strings.Contains(r.Value, "'") || strings.Contains(r.Value, "`") {
+			return errors.New(r.Name + ": characters \"' or ` are not allowed in TXT records")
+		}
 		// TODO: just check if there are not unsupported characters (" or `)
 	} else if r.Type == "SRV" {
-
 	} else if r.Type == "MX" {
 		if r.Prio <= 0 && r.Prio <= 100 {
 			return errors.New(r.Name + ": Prio has to be bigger than 0 and smaller than 100")
@@ -52,14 +58,17 @@ func (r *Record) Validate() error {
 	} else {
 		return errors.New("Unknown record type")
 	}
+
+	return nil
 }
 
 // Renders one record
 func (r *Record) Render() string {
 	if r.Type == "TXT" {
-		r.Value
+		// TODO: split the value
 	}
 
+	// TODO: prio only in case of MX record
 	return r.Name + "    " +
 		strconv.Itoa(r.TTL) + "s    " +
 		r.Type + "    " +
@@ -78,17 +87,36 @@ type Zone struct {
 // Validates records in the zone
 func (z *Zone) Validate() []error {
 	var errorsMsgs []error
+	var usedNames []string
 
 	for _, record := range z.Records {
 		err := record.Validate()
 		if err != nil {
 			errorsMsgs = append(errorsMsgs, err)
 		}
+
+		if record.Type == "A" || record.Type == "AAAA" || record.Type == "CNAME" {
+			usedNames = append(usedNames, record.Name)
+		}
 	}
 
 	// Additional checks
-	// TODO: A record can't have same name as another A record
-	// TODO: AAAA record can't have same name as another AAAA record
+
+	// CNAME record can't have same name as another AAAA record, A record or CNAME record
+	for _, record := range z.Records {
+		if record.Type == "CNAME" {
+			count := 0
+			for _, usedName := range usedNames {
+				if usedName == record.Name {
+					count += 1
+				}
+			}
+			if count > 1 {
+				errorsMsgs = append(errorsMsgs, errors.New(record.Name + " is already used in another A/AAAA/CNAME record"))
+			}
+		}
+	}
+
 
 	return errorsMsgs
 }
